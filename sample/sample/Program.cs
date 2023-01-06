f6ecff8e9275b49d79be8e283c96c84b88e8a5bf
NizarAhamed1998 <ahamednizarh651@gmail.com>
2023-01-06 15:12:28 +0530

    add2num Completed

--- sample/sample/Program.cs
+++ sample/sample/Program.cs
@@ -10,8 +10,13 @@ namespace sample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(add2num(10,20));
         }
         #region add_two_number
+        public static int add2num(int a, int b)
+        {
+            return a + b;
+        }
 
         #endregion
         #region Subtract_two_number
